7f54d684746e42541296406cc08c31ba5740c6b4
Aidan Kow <aidankow6@gmail.com>
2026-09-10 18:06:11 +0800

    bug fix: set mediapipe version to @1.0.1 instead of @latest

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -68,7 +68,7 @@ function App() {
         }
 
         const vision = await FilesetResolver.forVisionTasks(
-          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+          "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@1.0.1/wasm"
         );
 
         handLandmarkerRef.current = await HandLandmarker.createFromOptions(vision, {
